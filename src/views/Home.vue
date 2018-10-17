974ee6e35b849526a8efe6cc6c654f1b5eab48d3
Niko Savas <niko@savas.ca>
2018-10-16 20:14:41 -0700

    Updated the homepage to be more clear about what scrobblify is for

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="home">
-    <h1>Want to scrobble your last 2 weeks of spotify listens to last.fm?</h1>
-    Well you've come to the right place! Just follow these simples steps to get up and running with last.fm
+    <h1>So you want to start tracking your spotify plays...</h1>
+    You've come to the right place!
     <br>
-    <h2>1. Request to download your Spotify listening history</h2>
+    <h2>How to connect your last.fm and spotify accounts</h2>
+    <ol>
+      <li>Go to <a href="https://www.last.fm/join">https://www.last.fm/join</a> and create a last.fm account.</li>
+      <li>Go to <a href="https://www.last.fm/settings/applications">https://www.last.fm/settings/applications</a> and connect your accounts.</li>
+    </ol>
+    <br>
+    Now all your plays will be automatically scrobbled (saved) to last.fm! You can use last.fm to track your listening history and use cool tools like <a href="https://savas.ca/lastwave">LastWave</a>.
+
+    <h2>But I want to see cool graphs right now!</h2>
+    Well you're in luck - Scrobblify to the rescue! Scrobblify lets you retroactively scrobble your past 2 weeks of plays from spotify straight to last.fm.
+    <br><br>
+    <b>1. Request to download your Spotify listening history</b><br>
     Go to <a href="https://www.spotify.com/ca-en/account/privacy/">https://www.spotify.com/ca-en/account/privacy/</a>,
     at the bottom click "Request" to request your personal data file. They say it can take up to 30 days but I've found
-    it usually only takes a couple days.
-    <h2>2. Receive email from Spotify that your data is ready</h2>
+    it usually only takes a couple days.<br>
+    <b>2. Receive email from Spotify that your data is ready</b><br>
     Go back to <a href="https://www.spotify.com/ca-en/account/privacy/">https://www.spotify.com/ca-en/account/privacy/</a>
-    and download your zip file.
-    <h2>3. Create a last.fm account</h2>
+    and download your zip file.<br>
+    <b>3. Create a last.fm account</b><br>
     Create a new account <a href="https://www.last.fm/join">here</a> (an existing account will also work - scrobblify
-    will check to make sure it doesn't add any duplicate scrobbles).
-    <h2>4. Get your data ready</h2>
-    Unzip <b>my_spotify_data.zip</b> and open <b>StreamingHistory.json</b> in a text editor (notepad will work fine).
-    <h2>5. Use scrobblify!</h2>
+    will check to make sure it doesn't add any duplicate scrobbles).<br>
+    <b>4. Get your data ready</b><br>
+    Unzip <code>my_spotify_data.zip</code> and open <code>StreamingHistory.json</code> in a text editor (notepad will work fine).<br>
+    <b>5. Use scrobblify!</b><br>
     Go to <router-link to="/scrobble">Scrobble</router-link> and follow the steps there.
   </div>
 </template>
