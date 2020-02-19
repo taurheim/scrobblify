ee3226675c77962747d713ff9cae7262279c362a
Nikolai Savas <nsavas@microsoft.com>
2020-02-18 21:21:36 -0800

    Upgrade packages

---
+++ src/models/RateLimitError.ts
@@ -0,0 +1,6 @@
+export default class RateLimitError extends Error {
+    constructor(message: string) {
+        super(message);
+        Object.setPrototypeOf(this, RateLimitError.prototype);
+    }
+}
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  publicPath: (process.env.NODE_ENV === 'production') ? '/scrobblify' : '/',
-  outputDir: 'dist/scrobblify',
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
-};
+  "publicPath": "/",
+  "outputDir": "dist/scrobblify",
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
